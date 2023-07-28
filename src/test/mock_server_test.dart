822165c5e93815f16930fc8f40c0e547dc0a2aef
tomoki <>
2023-07-28 22:48:20 +0900

    - モックサーバーのレスポンスを確認するテストを実装

--- src/test/mock_server_test.dart
+++ src/test/mock_server_test.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:src/mock_server/mock_server.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Mock Server', () {
-    MockServer server = MockServer();
+    test('id', () async {
+      final jsonString = await MockServer.searchByKeyword("");
+      expect(jsonString["items"][0]["id"], 3081286);
+    });
   });
 }
